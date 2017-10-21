7cf176a68f512d2e9294bd4cb3903f4a19a424e2
JulianZY <zhuyong_zyz@yeah.net>
2017-10-21 22:14:34 +0800

    [CHG] resolve database mapping problem

--- src/test/java/com/test/testsys/service/ChoiceServciceTest.java
+++ src/test/java/com/test/testsys/service/ChoiceServciceTest.java
@@ -39,11 +39,11 @@ public class ChoiceServciceTest {
 		int size = 10;
 		for(int i = 0;i < size;i++) {
 			c = new Choice();
-			c.setChoiceA("哈哈哈" + i);
-			c.setChoiceB("呵呵呵" + i);
-			c.setChoiceC("好好好" + i);
-			c.setChoiceD("哼哼哼" + i);
-			c.setQuestionText("question" + i);
+			c.setChoiceA("哈哈哈" + i + i);
+			c.setChoiceB("呵呵呵" + i + i);
+			c.setChoiceC("好好好" + i + i);
+			c.setChoiceD("哼哼哼" + i + i);
+			c.setQuestionText("question" + i + i);
 			c.setRightAnswer("A");
 			result = choiceService.addQuestion(c);
 			System.out.println(result.getCode() + "," + result.getMsg());
@@ -56,7 +56,9 @@ public class ChoiceServciceTest {
 		List<Choice> result = null;
 		List<Integer> ids = new ArrayList<Integer>();
 		ids.add(1);
-		result = choiceService.queryQuestionsByIds(ids);
+		List<String> uuids = new ArrayList<String>();
+		uuids.add("307e2d56-4ca9-4fc2-a40f-eb2d23a7fabf");
+		result = choiceService.queryQuestionsByUuids(uuids);
 		System.out.println("over3");
 	}
 	
@@ -64,16 +66,21 @@ public class ChoiceServciceTest {
 	public void testDelete() {
 		List<Choice> result = null;
 		List<Integer> ids = new ArrayList<Integer>();
-		ids.add(2);
-		choiceService.deleteQuestionsByIds(ids);
+		ids.add(4);
+		List<String> uuids = new ArrayList<String>();
+		uuids.add("003938ed-7bcb-4c5f-8e1f-ff9342ad4429");
+		choiceService.deleteQuestionsByUuids(uuids);
 		System.out.println("over4");
 	}
 	
 	@Test
 	public void testUpdate() {
-		List<Choice> result = choiceService.queryAllQuestions();
+		List<Choice> results = choiceService.queryAllQuestions();
+		List<String> uuids = new ArrayList<String>();
+		uuids.add("4369c098-efef-4959-abff-9e9991cc1fd8");
+		List<Choice> result = choiceService.queryQuestionsByUuids(uuids);
 		Choice c = result.get(0);
-		c.setQuestionText("question-update");
+		c.setQuestionText("question-update222");
 		ResultData resultd = choiceService.addQuestion(c);
 		System.out.println("over5");
 	}
